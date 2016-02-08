dd3de5ba5aef807ab17c92cf520891884e7c1b37
AhmadAT86 <ah.aboutaouk@gmail.com>
2016-02-08 16:38:10 +0100

    La till StyleSheet.css och _reference.js men insåg att jag började fel. Dem är inte i funktionella för tillfället, ja måste först skapa Id (Created samt htmlattribute).  Ska även lägga till Datepicker i (Created). Måste även definiera ProjectOnwer så att den kommer med på sidan

--- CombiTech.Solution/CombiTech.Solution/Models/ToDo.cs
+++ CombiTech.Solution/CombiTech.Solution/Models/ToDo.cs
@@ -5,15 +5,21 @@ using System.Web;
 
 namespace CombiTech.Solution.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class ToDo
     {
+        [Required]
         public int Id { get; set; }
         public string Description { get; set; }
+        public string ProjectOnwer { get; set; }
         public DateTime Created { get; set; }
         public bool Active { get; set; }
 
     }
 }
+
+
 //public int Id { get; set; }
 //public int PersonId { get; set; }
 //public string Description { get; set; }
